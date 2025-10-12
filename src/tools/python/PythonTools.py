c9ec7a302809e89f2d1b15512f44fbc53b7edb72
liuyanjie <liuyanjie2@qq.com>
2025-10-13 00:26:21 +0800

    添加我的选股策略

---
+++ src/tools/python/PythonTools.py
@@ -0,0 +1,17 @@
+print("你好1")
+
+text = "你好2"
+print(text)
+
+def main():
+    text2 = "你好3"
+    vv = True
+    cc = None
+    if vv:
+        print("vv是True")
+    else:
+        print("vv是False")
+    print(text2)
+    print(cc)
+
+main()
